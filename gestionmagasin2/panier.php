980ca9f0c72314c7fafd55726ab585641f8c2709
khaoulald <lessaneddine.khaoula.solicode@gmail.com>
2022-03-22 11:06:27 +0100

    gestionmagasin

--- gestionmagasin2/panier.php
+++ gestionmagasin2/panier.php
@@ -59,7 +59,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="panier.css" rel="stylesheet">
+    <link href="frontend/panier.css" rel="stylesheet">
     <title>Panier</title>
 </head>
 <body>
